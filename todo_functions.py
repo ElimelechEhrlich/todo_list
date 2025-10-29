5778080ff639486ec43cda51283458612d073b6a
Elimelech Ehrlich <ee525566@gmail.com>
2025-10-29 13:52:37 +0200

    create main_functions

--- todo_functions.py
+++ todo_functions.py
@@ -1,34 +1,63 @@
-todo_list = ['aaa', 'bbb']
+todo_list = []
 
-def added_tesk(task:str, tasks:list):
-    return tasks.append(task)
-    # return 'The task was added successfully.'
+def added_tesk(tasks:list):
+    task = str(input('new task:'))
+    tasks.append(task)
+    print ('The task was added successfully.')
+    return
 
 def view_tasks(tasks:list):
     if len(tasks) == 0:
         return 'The task lists are empty.\n' \
-        'If you want to add a task - select the Add task option'
+        'If you want to add a task - select the Add task option\n'
+    print ('\ntasks:')
     for i in range(len(tasks)):
-        print (f'{i+1}. {tasks[i]}')
+        print (f'\n{i+1}. {tasks[i]}')
     return  
 
-def options():
-    options_list = {'1': 'Add task',
-               '2' : 'View tasks',
-               '3' : 'Delete task',
-               '4' : 'Edit task',
-               '5' : 'Exit'
-               }
-    for i in options_list:
-        print (f'{i}. {options_list[i]}')
-    return options_list
+def delete_task(tasks:list):
+    return 'This action is not yet available.'
 
-def user_selection(options):
+def edited_task(tasks:list):
+    return 'This action is not yet available.'
+
+def brake(tasks:list):
+    return 'End of task organization'
+
+def options():
+    options = {1: ('Add task', added_tesk),
+               2 : ('View tasks', view_tasks),
+               3 : ('Delete task', delete_task),
+               4 : ('Edit task', edited_task),
+               5 : ('Exit', brake)
+               }
+    return options
+
+def view_options(options:dict[tuple]):
+    print ('\nThe options are:')
+    for i in options:
+        print (f'\n{i}. {options[i][0]}')
+    return
+
+def user_selection(options:dict):
     selection = int(input('Select an action from the options.'))
-    if selection < 1 or selection > len(options):
+    while selection < 1 or selection > len(options):
         print ('Write the correct operation number according to the list.')
         selection = user_selection(options)
     return selection
 
-print (user_selection(options()))
+def main() -> None:
+    global todo_list
+    tasks = todo_list
+    while True:
+        print (view_options(options()))
+        selection = user_selection(options())
+        if selection == 5:
+            return
+        selected_action = options()[selection][1]
+        selected_action(tasks)
+    return
+main()
+    
+
 
