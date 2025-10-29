7348ee7d20570bf6326b9c7f3a3addb61b1b18f5
Elimelech Ehrlich <ee525566@gmail.com>
2025-10-29 11:19:06 +0200

    create view_tasks function

--- todo_functions.py
+++ todo_functions.py
@@ -1,6 +1,17 @@
-todo_list = []
+todo_list = ['aaa', 'bbb']
 
-def added_tesk(tesk:str, tesk_list:list):
-    return tesk_list.append(tesk)
+def added_tesk(tesk:str, tesks:list):
+    return tesks.append(tesk)
     # return 'The task was added successfully.'
+
+def view_tasks(tesks:list):
+    if len(tesks) == 0:
+        return 'The task lists are empty.\n' \
+        'If you want to add a task - select the Add task option'
+    for i in range(len(tesks)):
+        print (f'{i+1}. {tesks[i]}')
+    return  
+
+
+
 
